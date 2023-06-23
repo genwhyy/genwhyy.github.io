91da1e8e42318571d203b467af75d37acbf6165a
Alissa <75789938+genwhyy@users.noreply.github.com>
2023-06-23 16:57:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ slides[0].classList.add('active');
 //slider
 function changeSlide(){ // выносим смену первого и второго слайда в отдельную фукнцию, чтобы не повторять код дважды
     if(slides[0].classList.contains("active")){ // если первый слайд активный
-        slides[0].classList.remove("active"); // больше не активный
-        slides[1].classList.add("active"); // а второй активный)0)
+        slides[0].classList.remove("active");
+        slides[1].classList.add("active"); 
     }
-    else{ // иначе
-        slides[0].classList.add("active"); // первый активни
-        slides[1].classList.remove("active"); // а второй нит)
+    else{ 
+        slides[0].classList.add("active"); 
+        slides[1].classList.remove("active"); 
     }
 };
 prevBtn.addEventListener("click", (e)=>{ // предыдущийСлайдКнопка вешать ивент слушатель на щелкчок
