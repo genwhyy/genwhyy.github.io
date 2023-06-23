d5f21c0065541e0ef1cbf0cbc3347eeb03534bb8
Alissa <75789938+genwhyy@users.noreply.github.com>
2023-06-23 16:33:38 +0600

    Update contactsScript.js

--- contactsScript.js
+++ contactsScript.js
@@ -3,7 +3,7 @@ const message = document.querySelector('#submit');
 function accept(){
     alert('Sent!!');
 }
-message.addEventListener("click", (e)=>{ // предыдущийСлайдКнопка вешать ивент слушатель на щелкчок
-    e.preventDefault(); // обнуляем(0) все стандартные обработчики событий (iventzz)
-    accept(); // вызываем смену слайда
+message.addEventListener("click", (e)=>{
+    e.preventDefault(); 
+    accept(); 
 });
